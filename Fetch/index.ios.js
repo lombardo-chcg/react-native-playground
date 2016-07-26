8b6d77642dd8889a9ef4207dcb59497a5bc433e3
lombardo-chcg <dpa.lombardo@gmail.com>
2016-07-26 14:16:55 -0500

    Init ios project and basic api fetch

---
+++ Fetch/index.ios.js
@@ -0,0 +1,91 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Fetch extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: undefined
+    }
+  }
+
+  componentDidMount() {
+    console.log('c-d-m')
+    this.setState({ 
+      data: this.getMoviesFromApiAsync()
+    })
+  }
+
+  getMoviesFromApiAsync() {
+    return fetch('//gentle-lowlands-31515.herokuapp.com/test')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.movies;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }    
+
+  render() {
+    if (this.state.data === undefined) {
+      console.log(this.state.data, "if")      
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+        </View>
+      );
+    } else {
+      console.log(this.state.data, "else")
+      return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            api done
+          </Text>
+        </View>         
+      )
+    }  
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('Fetch', () => Fetch);
