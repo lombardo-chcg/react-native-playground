f8c722f04029dce848798498ba56b52d069d37f9
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-28 18:21:32 -0500

    stateful annotations

--- MapSource/index.ios.js
+++ MapSource/index.ios.js
@@ -35,18 +35,42 @@ class MapSource extends Component {
 
   render() {
     return (
-      <MapView
-        annotations={[this.state.pin]}
-        style={styles.map} 
-        onRegionChangeComplete={this.onRegionChangeComplete.bind(this)}
-      />
+      <View style={styles.container}>
+        <MapView
+          annotations={[this.state.pin]}
+          style={styles.map} 
+          onRegionChangeComplete={this.onRegionChangeComplete.bind(this)}
+        />
+        <View style={styles.textWrapper}>
+          <Text style={styles.text} >latitude: {this.state.pin.latitude}</Text>
+          <Text style={styles.text}>longitude: {this.state.pin.longitude}</Text>
+          <Text style={styles.text}>cool map bro</Text>
+        </View>        
+      </View>  
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#F5FcFF'
+  },
+
   map: {
-    flex: 1
+    flex: 3,
+    marginTop: 30
+  },
+
+  textWrapper: {
+    flex: 1,
+    alignItems: 'center',  
+  },
+
+  text: {
+    fontSize: 30
   }
 });
 
