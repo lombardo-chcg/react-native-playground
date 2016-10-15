97836a1b3ee9c49c26f1d3e28922ea636be90fdf
lombardo-chcg <dpa.lombardo@gmail.com>
2016-10-14 22:31:46 -0500

    auth functionality complete

--- Scheduler/src/components/LoginForm.js
+++ Scheduler/src/components/LoginForm.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { connect } from 'react-redux';
-import { Card, CardSection, Input, Button } from './common';
-import { emailChanged } from '../actions';
+import { Card, CardSection, Input, Button, Spinner } from './common';
+import { emailChanged, logInUser, passwordChanged } from '../actions';
 
 class LoginForm extends Component {
 	onEmailChange(text) {
+		this.props.emailChanged(text);
+	}
 
+	onPasswordChange(text) {
+		this.props.passwordChanged(text);
+	}
+
+	onButtonPress() {
+		const { email, password } = this.props;
+
+		this.props.logInUser({email, password})
+	}
+
+	renderButton() {
+		if (this.props.loading) {
+			return <Spinner size="large"/>;
+		}
+
+		return(
+			<Button onPress={this.onButtonPress.bind(this)}>
+				Login
+			</Button>		
+		);
 	}
 
 	render() {
@@ -16,6 +39,7 @@ class LoginForm extends Component {
 						label='email'
 						placeholder='you@mail.com'
 						onChangeText={this.onEmailChange.bind(this)}
+						value={this.props.email}
 					/>
 				</CardSection>
 
@@ -24,17 +48,46 @@ class LoginForm extends Component {
 						secureTextEntry
 						label='password'
 						placeholder='password'
-					/>				
+						onChangeText={this.onPasswordChange.bind(this)}
+						value={this.props.password}
+					/>					
 				</CardSection>
 
+				<Text style={styles.errorTextStyle}>
+					{this.props.error}
+				</Text>
+				
 				<CardSection>
-					<Button>
-						Login
-					</Button>
+					{this.renderButton()}
 				</CardSection>				
 			</Card>
 		);
 	}
 }
 
-export default connect(null, { emailChanged })(LoginForm);
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	} 
+}
+
+const mapStateToProps = (state) => {
+	return { 
+		email: state.auth.email,
+		password: state.auth.password,
+		error: state.auth.error,
+		loading: state.auth.loading
+	}
+};
+
+export default connect(mapStateToProps, { emailChanged, logInUser, passwordChanged })(LoginForm);
+
+
+
+
+
+
+
+
--- Scheduler/src/reducers/AuthReducer.js
+++ Scheduler/src/reducers/AuthReducer.js
@@ -1,8 +1,40 @@
-const INITIAL_STATE = {email: ''};
+import { 
+	EMAIL_CHANGED, 
+	PASSWORD_CHANGED,
+	LOGIN_USER_ATTEMPT_STARTED,
+	LOGIN_USER_SUCCESS,
+	LOGIN_USER_FAIL
+} from '../actions/types';
 
-export default (state = INITIAL_STATE, action) => {
-	switch(action.type) {
+const INITIAL_STATE = {
+	email: '', 
+	password: '',
+	loading: false,
+	error: '',
+	user: null
+};
+
+const AuthReducer = (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		case EMAIL_CHANGED:
+			return {...state, email: action.payload};
+
+		case PASSWORD_CHANGED:
+			return {...state, password: action.payload};
+
+		case LOGIN_USER_ATTEMPT_STARTED:
+			return {...state, loading: true, error: '',
+			};
+
+		case LOGIN_USER_SUCCESS:
+			return {...state, ...INITIAL_STATE, user: action.payload};
+
+		case LOGIN_USER_FAIL:
+			return {...state, error: 'TOTAL FAIL', password: '', loading: false}
+
 		default: 
 			return state;
 	}
 }
+
+export default AuthReducer;
